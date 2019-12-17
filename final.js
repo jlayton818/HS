e99408944386879e14dfe07b4b551bca6b93a3e2
Jonah Layton <jlayton818@gmail.com>
2019-12-17 09:01:18 -0700

    All variables listed

--- final.js
+++ final.js
@@ -1,4 +1,3 @@
-
 function ready() {
     console.log("Page Ready")
     let uscap = document.forms.uscap;  // favs is the name of the form
@@ -7,75 +6,226 @@ function ready() {
     let alcap = document.forms.alcap;  // favs is the name of the form
     let Alcapital = alcap.elements.alcapital;
     let capitalAl = alcap.elements.alcapcheck;
-    // let akcap = document.forms.akcap;
-    // let capital = akcap.elements.akcapital;
-    // let capitalak = akcap.elements.akcapcheck;
-    // let azcap = document.forms.azcap;
-    // let azcapital = azcap.elements.capital;
-    // let capitalaz = azcap.elements.azcapcheck;      
-    // let arcap = document.forms.arcap;
-    // let arcapital = arcap.elements.capital;
-    // let capitalar = arcap.elements.akcapcheck;   
-    // let cacap = document.forms.cacap;
-    // let cacapital = cacap.elements.capital;
-    // let capitalca = cacap.elements.cacapcheck;   
-    // let cocap = document.forms.cocap;
-    // let cocapital = cocap.elements.capital;
-    // let capitalco = cocap.elements.cocapcheck;   
-    // let ctcap = document.forms.ctcap;
-    // let ctcapital = ctcap.elements.capital;
-    // let capitalct = ctcap.elements.ctcapcheck;   
-    // let akcap = document.forms.akcap;
+    let akcap = document.forms.akcap;
+    let akcapital = akcap.elements.akcapital;
+    let capitalak = akcap.elements.akcapcheck;
+    let azcap = document.forms.azcap;
+    let azcapital = azcap.elements.capital;
+    let capitalaz = azcap.elements.azcapcheck;      
+    let arcap = document.forms.arcap;
+    let arcapital = arcap.elements.capital;
+    let capitalar = arcap.elements.akcapcheck;   
+    let cacap = document.forms.cacap;
+    let cacapital = cacap.elements.capital;
+    let capitalca = cacap.elements.cacapcheck;   
+    let cocap = document.forms.cocap;
+    let cocapital = cocap.elements.capital;
+    let capitalco = cocap.elements.cocapcheck;   
+    let ctcap = document.forms.ctcap;
+    let ctcapital = ctcap.elements.capital;
+    let capitalct = ctcap.elements.ctcapcheck;   
+    let decap = document.forms.decap;
+    let decapital = decap.elements.capital;
+    let capitalde = decap.elements.decapcheck;   
+    let flcap = document.forms.flcap;
+    let flcapital = flcap.elements.capital;
+    let capitalfl = flcap.elements.flcapcheck; 
+    let gacap = document.forms.gacap;  
+    let gacapital = gacap.elements.capital;
+    let capitalga = gacap.elements.gacapcheck;
+    let hicap = document.forms.hicap;   
+    let hicapital = hicap.elements.capital;
+    let capitalhi = hicap.elements.hicapcheck;    
+    let idcap = document.forms.idcap;   
+    let idcapital = idcap.elements.capital;
+    let capitalid = idcap.elements.idcapcheck;   
+    let ilcap = document.forms.ilcap;   
+    let ilcapital = ilcap.elements.capital;
+    let capitalil = ilcap.elements.ilcapcheck;   
+    let incap = document.forms.INcap;   
+    let incapital = incap.elements.capital;
+    let capitalin = incap.elements.incapcheck;   
+    let iacap = document.forms.iacap;   
+    let iacapital = iacap.elements.capital;
+    let capitalia = iacap.elements.iacapcheck;   
+    let kscap = document.forms.kscap;   
+    let kscapital = kscap.elements.capital;
+    let capitalks = kscap.elements.kscapcheck;   
+    let kycap = document.forms.kycap;   
+    let kycapital = kycap.elements.capital;
+    let capitalky = kycap.elements.kycapcheck;   
+    let lacap = document.forms.lacap;   
+    let lacapital = lacap.elements.capital;
+    let capitalla = lacap.elements.lacapcheck;   
+    let mecap = document.forms.mecap;   
+    let mecapital = mecap.elements.capital;
+    let capitalme = mecap.elements.mecapcheck;   
+    let mdcap = document.forms.mdcap;   
+    let mdcapital = mdcap.elements.capital;
+    let capitalmd = mdcap.elements.mdcapcheck; 
+    let macap = document.forms.macap;   
+    let macapital = macap.elements.capital;
+    let capitalma = macap.elements.macapcheck;  
+    let micap = document.forms.micap;   
+    let micapital = micap.elements.capital;
+    let capitalmi = micap.elements.micapcheck;
+    let mncap = document.forms.mncap;   
+    let mncapital = mncap.elements.capital;
+    let capitalmn = mncap.elements.mncapcheck;
+    let mscap = document.forms.mscap;   
+    let mscapital = mscap.elements.capital;
+    let capitalms = mscap.elements.mscapcheck;
+    let mocap = document.forms.mocap;   
+    let mocapital = mocap.elements.capital;
+    let capitalmo = mocap.elements.mocapcheck;
+    let mtcap = document.forms.mtcap;   
+    let mtcapital = mtcap.elements.capital;
+    let capitalmt = mtcap.elements.mtcapcheck;
+    let necap = document.forms.necap;   
+    let necapital = necap.elements.capital;
+    let capitalne = necap.elements.necapcheck;
+    let nvcap = document.forms.nvcap;   
+    let nvcapital = nvcap.elements.capital;
+    let capitalnv = nvcap.elements.nvcapcheck;
+    let nhcap = document.forms.nhcap;   
+    let nhcapital = nhcap.elements.capital;
+    let capitalnh = nhcap.elements.nhcapcheck;
+    let njcap = document.forms.njcap;   
+    let njcapital = njcap.elements.capital;
+    let capitalnj = njcap.elements.njcapcheck;
+    let nmcap = document.forms.nmcap;   
+    let nmcapital = nmcap.elements.capital;
+    let capitalnm = nmcap.elements.nmcapcheck;
+    let nycap = document.forms.nycap;   
+    let nycapital = nycap.elements.capital;
+    let capitalny = nycap.elements.nycapcheck;
+    let nccap = document.forms.nccap;   
+    let nccapital = nccap.elements.capital;
+    let capitalnc = nccap.elements.nccapcheck;
+    let ndcap = document.forms.ndcap;   
+    let ndcapital = ndcap.elements.capital;
+    let capitalnd = ndcap.elements.ndcapcheck;
+    let ohcap = document.forms.ohcap;   
+    let ohcapital = ohcap.elements.capital;
+    let capitaloh = ohcap.elements.ohcapcheck;
+    let okcap = document.forms.okcap;   
+    let okcapital = okcap.elements.capital;
+    let capitalok = okcap.elements.okcapcheck;
+    let orcap = document.forms.orcap;   
+    let orcapital = orcap.elements.capital;
+    let capitalor = orcap.elements.orcapcheck;
+    let pacap = document.forms.pacap;   
+    let pacapital = pacap.elements.capital;
+    let capitalpa = pacap.elements.pacapcheck;
+    let ricap = document.forms.ricap;   
+    let ricapital = ricap.elements.capital;
+    let capitalri = ricap.elements.ricapcheck;
+    let sccap = document.forms.sccap;   
+    let sccapital = sccap.elements.capital;
+    let capitalsc = sccap.elements.sccapcheck;
+    let sdcap = document.forms.sdcap;   
+    let sdcapital = sdcap.elements.capital;
+    let capitalsd = sdcap.elements.sdcapcheck;
+    let tncap = document.forms.tncap;   
+    let tncapital = tncap.elements.capital;
+    let capitaltn = tncap.elements.tncapcheck;
+    let txcap = document.forms.txcap;   
+    let txcapital = txcap.elements.capital;
+    let capitaltx = txcap.elements.txcapcheck;
+    let utcap = document.forms.utcap;   
+    let utcapital = utcap.elements.capital;
+    let capitalut = utcap.elements.utcapcheck;
+    let vtcap = document.forms.vtcap;   
+    let vtcapital = vtcap.elements.capital;
+    let capitalvt = vtcap.elements.vtcapcheck;
+    let vacap = document.forms.vacap;   
+    let vacapital = vacap.elements.capital;
+    let capitalva = vacap.elements.vacapcheck;
+    let wacap = document.forms.wacap;   
+    let wacapital = wacap.elements.capital;
+    let capitalwa = wacap.elements.wacapcheck;
+    let wvcap = document.forms.wvcap;   
+    let wvcapital = wvcap.elements.capital;
+    let capitalwv = wvcap.elements.wvcapcheck;
+    let wicap = document.forms.wicap;   
+    let wicapital = wicap.elements.capital;
+    let capitalwi = wicap.elements.wicapcheck;
+    let wycap = document.forms.wycap;   
+    let wycapital = wycap.elements.capital;
+    let capitalwy = wycap.elements.wycapcheck;
+
+
+
+    
     // let akcapital = akcap.elements.capital;
     // let capitalak = akcap.elements.akcapcheck;   
-    // let akcap = document.forms.akcap;
+    
     // let akcapital = akcap.elements.capital;
     // let capitalak = akcap.elements.akcapcheck;   
-    // let akcap = document.forms.akcap;
+    
     // let akcapital = akcap.elements.capital;
     // let capitalak = akcap.elements.akcapcheck;   
-    // let akcap = document.forms.akcap;
+    
     // let akcapital = akcap.elements.capital;
     // let capitalak = akcap.elements.akcapcheck;   
-    // let akcap = document.forms.akcap;
-    // let akcapital = akcap.elements.capital;
-    // let capitalak = akcap.elements.akcapcheck;   
-    // let akcap = document.forms.akcap;
-    // let akcapital = akcap.elements.capital;
-    // let capitalak = akcap.elements.akcapcheck;   
-    // let akcap = document.forms.akcap;
-    // let akcapital = akcap.elements.capital;
-    // let capitalak = akcap.elements.akcapcheck;   
-    // let akcap = document.forms.akcap;
-    // let akcapital = akcap.elements.capital;
-    // let capitalak = akcap.elements.akcapcheck;   
-    // let akcap = document.forms.akcap;
+    
     // let akcapital = akcap.elements.capital;
     // let capitalak = akcap.elements.akcapcheck;   
 
     // color is one of the input in the favs form
-    sub.onclick = function() {
-        if (capitalEl.value == "Washington D.C.") {
-            console.log("hide id")
+    // go.onclick = function() {  // go is a button on the page
+    //     if (capitalEl.value == capitalUS.value) {
+    //         console.log("hide id");
+    //         document.getElementById('textInput').className = "hide";
+    //     
+    //     } 
+    //     if (capitalEl.value !== capitalUS.value) {
+    //         console.log("submit button clicked");
+    //         capitalEl.value = "";
+    //         alert("The Capital is Washington D.C.");
+    //     }
+
+    //     alert("Please don't go!"); 
+    // }
+    let x = 0
+    submit.onclick = function() {
+        console.log("clicked")
+        if (capitalEl.value == capitalUS.value || capitalEl.value == "") {
+            console.log("hide id");
             document.getElementById('textInput').className = "hide";
-            score = score + 100;
-        } else {
-            alert("The capital is Washington D.C.");
-            score = score - 100;
+            x = x + 1;
+        } 
+        if (capitalEl.value !== capitalUS.value || capitalEl.value == "") {
+            console.log("submit button clicked");
+            capitalEl.value = "";
+            alert("The Capital is Washington D.C.");
+            x = x - 1;
         }
-        console.log("submit button clicked")
-        capitalEl.value = ""
-    }
-    sub.onclick = function() {
-        if (Alcapital.value == "Montgomery") {
-            console.log("hide id")
+        if (Alcapital.value == "Montgomery" || Alcapital.value == "") {
+            console.log("hide id");
             document.getElementById('altextInput').className = "hide";
-            score = score + 100;
-        } else {
-            alert("The capital is Montgomery")
+            x = x + 1;
+        } 
+        if (Alcapital.value !== "Montgomery" || Alcapital.value == "") {
+            console.log("submit button clicked");
+            Alcapital.value = "";
+            alert("The Capital is Montgomery");
         }
-        console.log("submit button clicked")
-        AlCapital.value = ""
+        if (akcapital.value == "Juneau" || akcapital.value == "" ) {
+            console.log("hide id");
+            document.getElementById('aktextInput').className = "hide";
+            akcapital.value = "";
+        } 
+        if (akcapital.value !== "Juneau" || akcapital.value == "") {
+            console.log("submit button clicked");
+            document.getElementById('aktextInput').className = "hide";
+            akcapital.value = "";
+            alert("The Capital is Juneau");
+        }
+        scoretext.innerHTML = "<h3>Score:" + y + "</h3>";
+        scoretext.append("work");
+        console.log("work");
+        x++
     }
         
        
@@ -120,146 +270,213 @@ function ready() {
     }
     fl.onclick = function() {
         console.log("Gators");
-        document.getElementById('catextInput').className="show";
+        document.getElementById('fltextInput').className="show";
     }
     ga.onclick = function() {
         console.log("Georgia");
-        document.getElementById('catextInput').className="show";
+        document.getElementById('gatextInput').className="show";
     }
     hi.onclick = function() {
         console.log("Hawaii");
-        document.getElementById('catextInput').className="show";
+        document.getElementById('hitextInput').className="show";
     }
     id.onclick = function() {
         console.log("Idaho");
-        document.getElementById('catextInput').className="show";
+        document.getElementById('idtextInput').className="show";
     }
     il.onclick = function() {
         console.log("illinouis");
-        document.getElementById('catextInput').className="show";
+        document.getElementById('iltextInput').className="show";
     }
     IN.onclick = function() {
         console.log("Indiana");
-        document.getElementById('catextInput').className="show";
+        document.getElementById('INtextInput').className="show";
     }
     ia.onclick = function() {
         console.log("iowa");
+        document.getElementById('iatextInput').className="show";
     }
     ks.onclick = function() {
         console.log("knasas");
+        document.getElementById('kstextInput').className="show";
     }
     ky.onclick = function() {
         console.log("kentucky");
+        document.getElementById('kytextInput').className="show";
     }
     la.onclick = function() {
         console.log("louisnia");
+        document.getElementById('latextInput').className="show";
     }
     me.onclick = function() {
         console.log("miane");
+        document.getElementById('metextInput').className="show";
     }
     md.onclick = function() {
         console.log("maryland");
+        document.getElementById('mdtextInput').className="show";
     }
     ma.onclick = function() {
         console.log("mass");
+        document.getElementById('matextInput').className="show";
     }
     mi.onclick = function() {
         console.log("michigan");
+        document.getElementById('mitextInput').className="show";
     }
     mn.onclick = function() {
         console.log("minnisota");
+        document.getElementById('mntextInput').className="show";
     }
     ms.onclick = function() {
         console.log("Ole miss");
+        document.getElementById('mstextInput').className="show";
     }
     mo.onclick = function() {
         console.log("mizzou");
+        document.getElementById('motextInput').className="show";
     }
     mt.onclick = function() {
         console.log("montant");
+        document.getElementById('mttextInput').className="show";
     }
     ne.onclick = function() {
         console.log("nebraska");
+        document.getElementById('netextInput').className="show";
     }
     nv.onclick = function() {
         console.log("nevada");
+        document.getElementById('nvtextInput').className="show";
     }
     nh.onclick = function() {
         console.log("new hamp");
+        document.getElementById('nhtextInput').className="show";
     }
     nj.onclick = function() {
         console.log("new jersey");
+        document.getElementById('njtextInput').className="show";
     }
     nm.onclick = function() {
         console.log("new mex");
+        document.getElementById('nmtextInput').className="show";
     }
     ny.onclick = function() {
         console.log("new yoek");
+        document.getElementById('nytextInput').className="show";
     }
     nc.onclick = function() {
         console.log("north carolina");
+        document.getElementById('nctextInput').className="show";
     }
     nd.onclick = function() {
         console.log("north dakota");
+        document.getElementById('ndtextInput').className="show";
     }
     oh.onclick = function() {
         console.log("OHio");
+        document.getElementById('ohtextInput').className="show";
     }
     ok.onclick = function() {
         console.log("Oklahoma");
+        document.getElementById('oktextInput').className="show";
     }
     or.onclick = function() {
         console.log("Oregon");
+        document.getElementById('ortextInput').className="show";
     }
     pa.onclick = function() {
         console.log("penn");
+        document.getElementById('patextInput').className="show";
     }
     ri.onclick = function() {
         console.log("Small");
+        document.getElementById('ritextInput').className="show";
     }
     sc.onclick = function() {
         console.log("South Carolina");
+        document.getElementById('sctextInput').className="show";
     }
     sd.onclick = function() {
         console.log("South Dakota");
+        document.getElementById('sdtextInput').className="show";
     }
     tn.onclick = function() {
         console.log("Tenn");
+        document.getElementById('tntextInput').className="show";
     }
     tx.onclick = function() {
         console.log("yee haw");
+        document.getElementById('txtextInput').className="show";
     }
     ut.onclick = function() {
         console.log("utah");
+        document.getElementById('uttextInput').className="show";
     }
     vt.onclick = function() {
         console.log("vermont");
+        document.getElementById('vttextInput').className="show";
     }
     va.onclick = function() {
         console.log("virginia");
+        document.getElementById('vatextInput').className="show";
     }
     wa.onclick = function() {
         console.log("wash");
+        document.getElementById('watextInput').className="show";
     }
     wv.onclick = function() {
         console.log("west virgiina");
+        document.getElementById('wvtextInput').className="show";
     }
     wi.onclick = function() {
         console.log("wisconsin");
+        document.getElementById('witextInput').className="show";
     }
     wy.onclick = function() {
         console.log("wyoming");
-    } 
+        document.getElementById('wytextInput').className="show";
+    }
+    let y = 0
+    setInterval (() => {
+        if (y % 12 == 0) {
+            document.body.style.color = "red"
+        }
+        if (y % 12 == 1) {
+            document.body.style.color = "orange"
+        }
+        if (y % 12 == 2) {
+            document.body.style.color = "yellow"
+        }
+        if (y % 12 == 3) {
+            document.body.style.color = "green"
+        }
+        if (y % 12 == 4) {
+            document.body.style.color = "blue"
+        }
+        if (y % 12 == 5) {
+            document.body.style.color = "indigo"
+        }
+        if (y % 12 == 6) {
+            document.body.style.color = "violet"
+        }
+        if (y % 12 == 7) {
+            document.body.style.color = "indigo"
+        }
+        if (y % 12 == 8) {
+            document.body.style.color = "blue"
+        }
+        if (y % 12 == 9) {
+            document.body.style.color = "green"
+        }
+        if (y % 12 == 10) {
+            document.body.style.color = "yellow"
+        }
+        if (y % 12 == 11) {
+            document.body.style.color = "orange"
+        }
+        y = y + 1
+    }, 1000)
 }   
-
-// function onButtonClick(){
-//     document.getElementById('textInput').className="show";
-//   } 
-
-let score = 0
-function Score(){
-    score++;
-}
 
 
 document.addEventListener("DOMContentLoaded", ready);
